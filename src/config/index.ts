4a4aff7d653948b5f85ae3fb6571b931f9b9f9b7
Ibukun Ekunwe <ekunweibukun159357@gmail.com>
2024-09-13 03:47:11 +0100

    setup basic config

---
+++ src/config/index.ts
@@ -0,0 +1,45 @@
+import * as crypto from 'crypto';
+
+export interface Config {
+  apiUrl: string;
+  websiteUrl: string;
+  port: number;
+  mongo: { uri: string };
+  cors: { origin: string[] | string };
+  jwt: { secret: string; issuer: string; audience: string; expiresIn: string };
+}
+
+export function configuration() {
+  const appName = 'template';
+  const apiUrl = 'https://api.template.com';
+  const websiteUrl = 'https://template.com';
+
+  const randomPort = crypto.randomInt(49_152, 65_535); // using 0 might be better;
+
+  const nodeEnv = process.env.NODE_ENV ?? 'development';
+  const isProduction = ['prod', 'production'].includes(nodeEnv?.trim());
+
+  const corsOrigin = isProduction
+    ? [websiteUrl, 'https://admin.socket.io']
+    : '*';
+
+  const config: Config = {
+    apiUrl,
+    websiteUrl,
+    port: parseInt(process.env.PORT) || randomPort,
+    mongo: {
+      uri: process.env.MONGO_URI || `mongodb://127.0.0.1:27017/${appName}`,
+    },
+    cors: {
+      origin: corsOrigin,
+    },
+    jwt: {
+      secret: process.env.JWT_SECRET || 'insecure',
+      issuer: process.env.JWT_ISSUER || apiUrl,
+      audience: process.env.JWT_AUDIENCE || websiteUrl,
+      expiresIn: process.env.JWT_EXPIRES_IN || '30d',
+    },
+  };
+
+  return config;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Config } from './config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get(ConfigService<Config, true>);
+
+  const port = configService.get('port', { infer: true });
+  const cors = configService.get('cors', { infer: true });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors({ origin: cors.origin });
+
+  await app.listen(port, () => {
+    console.log(`Api is listening on port ${port}.`);
+  });
 }
 bootstrap();
